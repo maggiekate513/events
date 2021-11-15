7ad2680957f4b95d12239d1d0b0fd1fe50a1157c
anonymous anonymous <maggie.striebich@kellogg.northwestern.edu>
2021-11-15 20:32:10 +0000

    Direct association between EventResource and CommentResource

--- app/resources/comment_resource.rb
+++ app/resources/comment_resource.rb
@@ -7,6 +7,8 @@ class CommentResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :event
+
   belongs_to :user
 
   # Indirect associations
--- app/resources/event_resource.rb
+++ app/resources/event_resource.rb
@@ -10,6 +10,8 @@ class EventResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :comments
+
   belongs_to :user
 
   # Indirect associations
